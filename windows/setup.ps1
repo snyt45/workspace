628c90542133ade18caad62a8f1503b362864730
yuta.sano <snyt45@gmail.com>
2024-01-01 23:11:59 +0900

    add todoist

--- windows/setup.ps1
+++ windows/setup.ps1
@@ -48,6 +48,7 @@ winget install DeepL.DeepL
 winget install Dropbox.Dropbox
 winget install SlackTechnologies.Slack
 winget install Zoom.Zoom
+winget install Doist.Todoist
 
 echo "ソフトウェアのインストールが完了しました。"
 
