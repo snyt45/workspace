2b5e2f751c8b7bbc373685559df0e96b473455aa
yuta.sano <snyt45@gmail.com>
2025-03-17 11:09:45 +0900

    shareXをインストール

--- windows/setup.ps1
+++ windows/setup.ps1
@@ -47,6 +47,7 @@ winget install DeepL.DeepL
 winget install Dropbox.Dropbox
 winget install SlackTechnologies.Slack
 winget install Zoom.Zoom
+winget install ShareX.ShareX
 
 echo "ソフトウェアのインストールが完了しました。"
 
