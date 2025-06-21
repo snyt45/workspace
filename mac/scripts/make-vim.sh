c1207e06af7bb6eddff093f6ef40ade6b4a30ee4
yuta.sano <snyt45@gmail.com>
2025-06-22 00:56:01 +0900

    update make-vim

--- mac/scripts/make-vim.sh
+++ mac/scripts/make-vim.sh
@@ -4,46 +4,52 @@
 # VimのHEADをビルドしてインストールする
 # ----
 
-# Variables {{{
-
 CURRENT_DIR=`dirname $0`
 CLONE_DIR=${HOME}/work
 ROOT_DIR=${CLONE_DIR}/vim
 MAIN_BRANCH=master
 
-# }}}
-
-# Main {{{
-
+# vimリポジトリをクローン
 if [ ! -d "${ROOT_DIR}" ]; then
   git clone https://github.com/vim/vim ${ROOT_DIR}
 fi
 
 cd ${ROOT_DIR}
 
+# 最新のHEADを取得
 git fetch --prune origin
-git rebase origin/${MAIN_BRANCH}
+git reset --hard origin/${MAIN_BRANCH}
 
 cd src/
 
+# ビルド環境を完全にクリーンアップ
 make distclean
 
-# prefixを指定しない場合は/usr/local/binにインストールされる
+# Vimのビルド設定（/usr/local/binにインストール）
+# --enable-cscope: ソースコード解析ツールのサポート
+# --enable-fail-if-missing: 安全なビルドのための設定
+# --with-features=huge: 最大機能セットでのビルド
+# --enable-multibyte: 日本語サポート
+# --with-compiledby=snyt45: ビルド者の識別情報
 ./configure \
   --enable-cscope \
   --enable-fail-if-missing \
   --with-features=huge \
   --enable-multibyte \
-  --with-compiledby=snyt45 \
+  --with-compiledby=snyt45
 
+# ソースコードをコンパイル（~/work/vim/src/vim に実行ファイルを生成）
 make
 
+# コンパイル済みファイルを /usr/local/bin/ にコピーしてシステム全体で使用可能にする
+# 関連ファイル（ヘルプ、設定等）も /usr/local/share/vim/ にコピーされる
 sudo make install
 
+# 元のディレクトリに戻る
 cd ${CURRENT_DIR}
 
-sudo ln -s /usr/local/bin/vim /usr/local/bin/vi
+# viコマンドでもvimを使えるようにシンボリックリンクを作成（既存のファイルは上書き）
+sudo ln -sf /usr/local/bin/vim /usr/local/bin/vi
 
+# インストールされたVimのバージョンと機能を確認
 vim --version
-
-# }}}
