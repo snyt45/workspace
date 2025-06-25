0976f887366ff61cbb60981a9195e5076b60bd53
yuta.sano <snyt45@gmail.com>
2025-06-25 23:40:24 +0900

    update

--- mac/scripts/link.sh
+++ mac/scripts/link.sh
@@ -6,8 +6,6 @@ DOTFILES_DIR="$HOME/.dotfiles/mac"
 
 # 作業データ共有用のディレクトリ
 mkdir -p "$HOME/work/"
-# 作業用コンテナで作業時のキャッシュを残すためのディレクトリ
-mkdir -p "$HOME/.shared_cache/"
 
 # Gitの設定
 if [ -f "$DOTFILES_DIR/git/.gitconfig" ]; then
@@ -22,14 +20,6 @@ mkdir -p "$HOME/.vim/autoload/"
 if [ ! -f "$HOME/.vim/autoload/plug.vim" ]; then
     curl -fLo "$HOME/.vim/autoload/plug.vim" --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 fi
-mkdir -p "$HOME/.vim/config/"
-if [ -f "$DOTFILES_DIR/vim/config/.ctrlp-launcher" ]; then
-    ln -sf "$DOTFILES_DIR/vim/config/.ctrlp-launcher" "$HOME/.vim/config/.ctrlp-launcher"
-fi
-
-# efm-langserverの設定
-mkdir -p "$HOME/.config/efm-langserver"
-ln -sf "$DOTFILES_DIR/efm-langserver/config.yaml" "$HOME/.config/efm-langserver/config.yaml"
 
 # Tmuxの設定
 if [ -f "$DOTFILES_DIR/tmux/.tmux.conf" ]; then
