f2afad254a25a09ded3e5874a1297e2e2d0445f3
yuta.sano <snyt45@gmail.com>
2026-03-12 13:43:52 +0900

    TelescopeのinsertモードでESC一発で閉じるように変更
    Created by author with Claude Code

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -217,10 +217,14 @@ require("lazy").setup({
     },
     config = function()
       local telescope = require("telescope")
+      local actions = require("telescope.actions")
       telescope.setup({
         defaults = {
           layout_strategy = "vertical",
           layout_config = { height = 0.9, width = 0.9 },
+          mappings = {
+            i = { ["<Esc>"] = actions.close },
+          },
         },
         pickers = {
           find_files = {
