ec4d7394d3141b8fa98dee13ccd3d343a3d873bc
yuta.sano <snyt45@gmail.com>
2026-03-12 11:59:31 +0900

    LSP diagnosticフロート表示のキーマップ追加（gl）
    Created by author with Claude Code

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -404,5 +404,6 @@ vim.api.nvim_create_autocmd("LspAttach", {
     vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
     vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
     vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
+    vim.keymap.set("n", "gl", vim.diagnostic.open_float, opts)
   end,
 })
