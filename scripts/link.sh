4149915d119d54ab002773f40c13c61927d9c98d
yuta.sano <snyt45@gmail.com>
2026-03-19 10:28:39 +0900

    starshipでtmuxセッションのインジケータを表示
    プロンプト先頭にグリッドアイコンを表示してtmux内であることを視認できるようにした
    
    Created by author with Claude Code

--- scripts/link.sh
+++ scripts/link.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env zsh
 
 DOTFILES_DIR="$HOME/.dotfiles"
-PACKAGES=(bin git nvim zsh karabiner claude mise tmux)
+PACKAGES=(bin git nvim zsh karabiner claude mise tmux starship)
 
 ok=0
 ng=0
