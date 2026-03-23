6d0f2c9dc8f551997ce7cd1c077c7a941b2d15eb
yuta.sano <snyt45@gmail.com>
2026-03-23 09:54:31 +0900

    Catppuccinのカラースキームでカスタムハイライト設定を追加

--- nvim/.config/nvim/lua/plugins/catppuccin.lua
+++ nvim/.config/nvim/lua/plugins/catppuccin.lua
@@ -6,6 +6,12 @@ return {
 		require("catppuccin").setup({
 			flavour = "mocha",
 			transparent_background = true,
+			custom_highlights = function(colors)
+				return {
+					LineNr = { fg = colors.overlay0 },
+					CursorLineNr = { fg = colors.lavender },
+				}
+			end,
 		})
 		vim.cmd.colorscheme("catppuccin")
 	end,
