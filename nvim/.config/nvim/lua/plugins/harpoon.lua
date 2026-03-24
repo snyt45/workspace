6ce08a75c507d419ff0f0b38957453e39fc4d06a
yuta.sano <snyt45@gmail.com>
2026-03-24 12:15:48 +0900

    Harpoonメニューのキーバインドを`,h`から`,f`に変更

--- nvim/.config/nvim/lua/plugins/harpoon.lua
+++ nvim/.config/nvim/lua/plugins/harpoon.lua
@@ -15,7 +15,7 @@ return {
 			harpoon:list():add()
 			vim.notify("Harpoon: added " .. vim.fn.expand("%:."), vim.log.levels.INFO)
 		end, { desc = "Harpoon追加" })
-		vim.keymap.set("n", "<leader>h", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end, { desc = "Harpoonメニュー" })
+		vim.keymap.set("n", "<leader>f", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end, { desc = "Harpoonメニュー" })
 
 		vim.keymap.set("n", "<leader>1", function() harpoon:list():select(1) end, { desc = "Harpoon 1" })
 		vim.keymap.set("n", "<leader>2", function() harpoon:list():select(2) end, { desc = "Harpoon 2" })
