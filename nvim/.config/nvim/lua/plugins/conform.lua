6205433f775ca6f97829828ddbc3607026a6eb1a
yuta.sano <snyt45@gmail.com>
2026-03-31 09:13:43 +0900

    conformプラグインにフォーマット無効化コマンドと条件付き自動フォーマット機能を追加

--- nvim/.config/nvim/lua/plugins/conform.lua
+++ nvim/.config/nvim/lua/plugins/conform.lua
@@ -1,6 +1,20 @@
 return {
 	"stevearc/conform.nvim",
 	event = "BufWritePre",
+	init = function()
+		vim.api.nvim_create_user_command("ConformDisable", function(args)
+			if args.bang then
+				vim.b.disable_autoformat = true
+			else
+				vim.g.disable_autoformat = true
+			end
+		end, { desc = "Disable format-on-save", bang = true })
+
+		vim.api.nvim_create_user_command("ConformEnable", function()
+			vim.b.disable_autoformat = false
+			vim.g.disable_autoformat = false
+		end, { desc = "Enable format-on-save" })
+	end,
 	config = function()
 		require("conform").setup({
 			formatters_by_ft = {
@@ -11,10 +25,12 @@ return {
 				json = { "prettierd", "prettier", stop_after_first = true },
 				markdown = { "prettierd", "prettier", stop_after_first = true },
 			},
-			format_on_save = {
-				timeout_ms = 2000,
-				lsp_format = "fallback",
-			},
+			format_on_save = function(bufnr)
+				if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+					return
+				end
+				return { timeout_ms = 2000, lsp_format = "fallback" }
+			end,
 		})
 	end,
 }
