e3f42cd384fdbbaf710561b4d563c92953657cd9
yuta.sano <snyt45@gmail.com>
2026-04-07 11:44:59 +0900

    gitsignsで現在行のgit blameを有効化

--- nvim/.config/nvim/lua/plugins/gitsigns.lua
+++ nvim/.config/nvim/lua/plugins/gitsigns.lua
@@ -2,6 +2,7 @@ return {
 	"lewis6991/gitsigns.nvim",
 	config = function()
 		require("gitsigns").setup({
+			current_line_blame = true,
 			signs = {
 				add          = { text = "+" },
 				change       = { text = ">" },
