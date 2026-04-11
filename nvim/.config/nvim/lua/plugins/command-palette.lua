6c3a45e152107ebd8adcc5e95b92659bf458a4c4
yuta.sano <snyt45@gmail.com>
2026-04-12 08:46:06 +0900

    grug-farのキーマップをcommand-paletteからプラグイン設定に移行

--- nvim/.config/nvim/lua/plugins/command-palette.lua
+++ nvim/.config/nvim/lua/plugins/command-palette.lua
@@ -82,39 +82,6 @@ local commands = {
 		end,
 	},
 	{
-		name = "[GrugFar] プロジェクト全体で検索・置換",
-		cmd = "GrugFar",
-		execute = function()
-			vim.cmd("GrugFar")
-		end,
-	},
-	{
-		name = "[GrugFar] カーソル下の単語で検索",
-		cmd = "GrugFar { search = <cword> }",
-		execute = function()
-			require("grug-far").open({ prefills = { search = vim.fn.expand("<cword>") } })
-		end,
-	},
-	{
-		name = "[GrugFar] 現在のディレクトリ内で検索・置換",
-		cmd = "GrugFar { paths = <dir> }",
-		execute = function()
-			require("grug-far").open({ prefills = { paths = vim.fn.expand("%:p:h") } })
-		end,
-	},
-	{
-		name = "[GrugFar] quickfixのファイル内で検索・置換",
-		cmd = "GrugFar { search = <last_search>, paths = <qflist> }",
-		execute = function()
-			require("grug-far").open({
-				prefills = {
-					search = vim.g.last_lsp_search or "",
-					paths = "<qflist>",
-				},
-			})
-		end,
-	},
-	{
 		name = "[Conform] 自動フォーマットを無効化",
 		cmd = "ConformDisable",
 		execute = function()
@@ -126,13 +93,6 @@ local commands = {
 		cmd = "ConformEnable",
 		execute = function()
 			vim.cmd("ConformEnable")
-		end,
-	},
-	{
-		name = "[Nav] quickfixをクリア",
-		cmd = "cexpr []",
-		execute = function()
-			vim.cmd("cexpr []")
 		end,
 	},
 }
@@ -167,30 +127,30 @@ local function open_keymaps()
 	end)
 
 	pickers
-		.new(themes.get_dropdown({ previewer = false }), {
-			prompt_title = "Keymaps",
-			finder = finders.new_table({
-				results = results,
-				entry_maker = function(entry)
-					return {
-						value = entry,
-						display = entry.display,
-						ordinal = entry.ordinal,
-					}
-				end,
-			}),
-			sorter = conf.generic_sorter({}),
-			attach_mappings = function(prompt_bufnr)
-				actions.select_default:replace(function()
-					local selection = action_state.get_selected_entry()
-					actions.close(prompt_bufnr)
-					local keys = vim.api.nvim_replace_termcodes(selection.value.lhs, true, false, true)
-					vim.api.nvim_feedkeys(keys, "m", false)
-				end)
-				return true
-			end,
-		})
-		:find()
+	    .new(themes.get_dropdown({ previewer = false }), {
+		    prompt_title = "Keymaps",
+		    finder = finders.new_table({
+			    results = results,
+			    entry_maker = function(entry)
+				    return {
+					    value = entry,
+					    display = entry.display,
+					    ordinal = entry.ordinal,
+				    }
+			    end,
+		    }),
+		    sorter = conf.generic_sorter({}),
+		    attach_mappings = function(prompt_bufnr)
+			    actions.select_default:replace(function()
+				    local selection = action_state.get_selected_entry()
+				    actions.close(prompt_bufnr)
+				    local keys = vim.api.nvim_replace_termcodes(selection.value.lhs, true, false, true)
+				    vim.api.nvim_feedkeys(keys, "m", false)
+			    end)
+			    return true
+		    end,
+	    })
+	    :find()
 end
 
 return {
@@ -206,40 +166,40 @@ return {
 			local previewers = require("telescope.previewers")
 
 			pickers
-				.new({
-					layout_strategy = "horizontal",
-					layout_config = { width = 0.6, height = 0.4, preview_width = 0.4 },
-				}, {
-					prompt_title = "Command Palette",
-					finder = finders.new_table({
-						results = commands,
-						entry_maker = function(entry)
-							return {
-								value = entry,
-								display = entry.name,
-								ordinal = entry.name,
-							}
-						end,
-					}),
-					sorter = conf.generic_sorter({}),
-					previewer = previewers.new_buffer_previewer({
-						title = "Command",
-						define_preview = function(self, entry)
-							vim.api.nvim_buf_set_lines(self.state.bufnr, 0, -1, false, {
-								":" .. entry.value.cmd,
-							})
-						end,
-					}),
-					attach_mappings = function(prompt_bufnr)
-						actions.select_default:replace(function()
-							local selection = action_state.get_selected_entry()
-							actions.close(prompt_bufnr)
-							selection.value.execute()
-						end)
-						return true
-					end,
-				})
-				:find()
+			    .new({
+				    layout_strategy = "horizontal",
+				    layout_config = { width = 0.6, height = 0.4, preview_width = 0.4 },
+			    }, {
+				    prompt_title = "Command Palette",
+				    finder = finders.new_table({
+					    results = commands,
+					    entry_maker = function(entry)
+						    return {
+							    value = entry,
+							    display = entry.name,
+							    ordinal = entry.name,
+						    }
+					    end,
+				    }),
+				    sorter = conf.generic_sorter({}),
+				    previewer = previewers.new_buffer_previewer({
+					    title = "Command",
+					    define_preview = function(self, entry)
+						    vim.api.nvim_buf_set_lines(self.state.bufnr, 0, -1, false, {
+							    ":" .. entry.value.cmd,
+						    })
+					    end,
+				    }),
+				    attach_mappings = function(prompt_bufnr)
+					    actions.select_default:replace(function()
+						    local selection = action_state.get_selected_entry()
+						    actions.close(prompt_bufnr)
+						    selection.value.execute()
+					    end)
+					    return true
+				    end,
+			    })
+			    :find()
 		end, { desc = "[CommandPalette] コマンドパレット" })
 
 		vim.keymap.set("n", "<leader>?", open_keymaps, { desc = "[CommandPalette] キーマップ一覧" })
