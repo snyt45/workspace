d753704d45b182254c9480c1788a1407ff051f72
yuta.sano <snyt45@gmail.com>
2026-04-12 18:02:38 +0900

    tmuxプラグイン設定をtelescope統合に変更し、インデント形式を修正

--- nvim/.config/nvim/lua/plugins/tmux.lua
+++ nvim/.config/nvim/lua/plugins/tmux.lua
@@ -1,7 +1,6 @@
 return {
-	"tmux",
-	virtual = true,
-	config = function()
+	"nvim-telescope/telescope.nvim",
+	init = function()
 		vim.api.nvim_create_user_command("TmuxSwitch", function()
 			local output = vim.fn.system("tmux ls -F '#{session_name}'")
 			if vim.v.shell_error ~= 0 then
@@ -22,25 +21,26 @@ return {
 			local action_state = require("telescope.actions.state")
 
 			pickers
-				.new(require("telescope.themes").get_dropdown({ previewer = false }), {
-					prompt_title = "Tmux Sessions",
-					finder = finders.new_table({ results = sessions }),
-					sorter = conf.generic_sorter({}),
-					attach_mappings = function(prompt_bufnr)
-						actions.select_default:replace(function()
-							actions.close(prompt_bufnr)
-							local selection = action_state.get_selected_entry()
-							if selection then
-								vim.fn.system("tmux switch-client -t " .. vim.fn.shellescape(selection[1]))
-								if vim.v.shell_error ~= 0 then
-									vim.notify("セッション切り替えに失敗しました", vim.log.levels.ERROR)
-								end
-							end
-						end)
-						return true
-					end,
-				})
-				:find()
+			    .new(require("telescope.themes").get_dropdown({ previewer = false }), {
+				    prompt_title = "Tmux Sessions",
+				    finder = finders.new_table({ results = sessions }),
+				    sorter = conf.generic_sorter({}),
+				    attach_mappings = function(prompt_bufnr)
+					    actions.select_default:replace(function()
+						    actions.close(prompt_bufnr)
+						    local selection = action_state.get_selected_entry()
+						    if selection then
+							    vim.fn.system("tmux switch-client -t " ..
+								    vim.fn.shellescape(selection[1]))
+							    if vim.v.shell_error ~= 0 then
+								    vim.notify("セッション切り替えに失敗しました", vim.log.levels.ERROR)
+							    end
+						    end
+					    end)
+					    return true
+				    end,
+			    })
+			    :find()
 		end, { desc = "[Tmux] セッション切り替え" })
 	end,
 }
