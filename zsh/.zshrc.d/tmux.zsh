6f424a405cb71f26e2a9378f0a4b57ce1ef02860
yuta.sano <snyt45@gmail.com>
2026-04-13 09:46:28 +0900

    tmuxの設定をNeovimからzshエイリアスに統一

--- zsh/.zshrc.d/tmux.zsh
+++ zsh/.zshrc.d/tmux.zsh
@@ -3,10 +3,4 @@ alias tn='tmux new -s'
 alias td='tmux detach'
 alias tl='tmux ls'
 alias tk='tmux kill-session -t'
-
-# tmux内で別セッションに切り替え
-ts() {
-  local session
-  session=$(tmux ls -F '#S' 2>/dev/null | fzf --prompt='switch> ' --height=40%)
-  [[ -n "$session" ]] && tmux switch-client -t "$session"
-}
+alias ts='tmux switch-client -t'
