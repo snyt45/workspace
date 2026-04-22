afe52e4cab74ed6e63ace3fd3f26b4dc9a2d46f1
yuta.sano <snyt45@gmail.com>
2026-04-22 10:18:33 +0900

    opencodeのtutorを追加

--- scripts/link.sh
+++ scripts/link.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env zsh
 
 DOTFILES_DIR="$HOME/.dotfiles"
-PACKAGES=(bin git nvim zsh karabiner claude mise tmux ghostty)
+PACKAGES=(bin git nvim zsh karabiner claude mise tmux ghostty opencode)
 
 ok=0
 ng=0
