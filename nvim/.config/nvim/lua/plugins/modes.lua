eb733439fdd5124af4687adc1ca38cf834eea56d
yuta.sano <snyt45@gmail.com>
2026-04-27 09:10:34 +0900

    modes.vimのcursorlineをneo-treeで有効にする

--- nvim/.config/nvim/lua/plugins/modes.lua
+++ nvim/.config/nvim/lua/plugins/modes.lua
@@ -3,5 +3,18 @@ return {
 	event = "VeryLazy",
 	opts = {
 		line_opacity = 0.5,
+		set_cursorline = true,
+		ignore = {
+			"NvimTree",
+			"lspinfo",
+			"packer",
+			"checkhealth",
+			"help",
+			"man",
+			"TelescopePrompt",
+			"TelescopeResults",
+			"!minifiles",
+			"!neo-tree",
+		},
 	},
 }
