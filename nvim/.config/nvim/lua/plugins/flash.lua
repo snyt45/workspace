290ed8fbdc52ab50de1631a01721f553654bdd1c
yuta.sano <snyt45@gmail.com>
2026-08-04 13:21:25 +0900

    nvim: flashのremote action(yr/dr/cr)を削除
    不使用のためoperator-pendingのrキーマップとcheatsheet記載を除去
    
    Created by author with Claude Code

--- nvim/.config/nvim/lua/plugins/flash.lua
+++ nvim/.config/nvim/lua/plugins/flash.lua
@@ -4,7 +4,6 @@ return {
 	opts = {},
 	keys = {
 		{ "s",     mode = { "n", "x", "o" }, function() require("flash").jump() end,   desc = "[Flash] ジャンプ" },
-		{ "r",     mode = "o",               function() require("flash").remote() end, desc = "[Flash] リモートジャンプ (operator)" },
 		{ "<c-s>", mode = "c",               function() require("flash").toggle() end, desc = "[Flash] /?検索時のトグル" },
 	},
 }
